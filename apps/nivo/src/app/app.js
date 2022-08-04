0600e55523dd397cf0290d25f9e2d188f3a4ae6c
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-08-04 15:57:58 -0400

    Remove generated welcome page

--- apps/nivo/src/app/app.js
+++ apps/nivo/src/app/app.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 const StyledApp = styled.div`
@@ -8,8 +7,6 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="nivo" />
-
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
