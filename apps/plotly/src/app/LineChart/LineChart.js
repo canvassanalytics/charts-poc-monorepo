45acb2870db7538a7b81512bc5585a303180e1a7
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-08-09 13:58:03 -0400

    Add LineChart

---
+++ apps/plotly/src/app/LineChart/LineChart.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+const StyledLineChart = styled.div`
+  color: pink;
+`;
+export function LineChart(props) {
+  return (
+    <StyledLineChart>
+      <h1>Welcome to LineChart!</h1>
+    </StyledLineChart>
+  );
+}
+export default LineChart;
