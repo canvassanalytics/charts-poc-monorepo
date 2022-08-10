d62690f3455b1c0702e9b75e3d608ceadc87acc7
Ayden <ayden.garrett@canvass.io>
2022-08-09 23:06:05 -0400

    install locally

--- tools/plugins/netlify/install-deps/index.js
+++ tools/plugins/netlify/install-deps/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     onPreBuild: ({ utils }) => {
         const currentProject = process.env.PROJECT_NAME;
-        utils.run.command(`cd apps/${currentProject} && pwd`);
-        // utils.run.command(`cd apps/${currentProject}`);
+        utils.run.command(`npm --prefix ./apps/${currentProject} install ./apps/${currentProject}`);
+        utils.run.command(`echo done`);
         // utils.run.command(`pwd`);
         // utils.run.command(`npm install`);
     },
