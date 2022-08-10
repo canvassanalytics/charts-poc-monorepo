52861d627167043440aa6128ce21abcde3d8e6a9
Ayden <ayden.garrett@canvass.io>
2022-08-09 23:01:43 -0400

    try again

--- tools/plugins/netlify/install-deps/index.js
+++ tools/plugins/netlify/install-deps/index.js
@@ -1,9 +1,9 @@
 module.exports = {
     onPreBuild: ({ utils }) => {
         const currentProject = process.env.PROJECT_NAME;
-        utils.run.command(`pwd`);
-        utils.run.command(`cd apps/${currentProject}`);
-        utils.run.command(`pwd`);
-        utils.run.command(`npm install`);
+        utils.run.command(`cd apps/${currentProject} && pwd`);
+        // utils.run.command(`cd apps/${currentProject}`);
+        // utils.run.command(`pwd`);
+        // utils.run.command(`npm install`);
     },
 };
