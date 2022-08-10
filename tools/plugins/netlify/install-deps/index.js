3d5c35db82ad6e21cccb78996e90c16e48d98c96
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-08-10 17:28:15 -0400

    Update deployment workflow

--- tools/plugins/netlify/install-deps/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    onPreBuild: ({ utils }) => {
-        const currentProject = process.env.PROJECT_NAME;
-        utils.run.command(`npm install ./apps/${currentProject}`);
-    },
-};
