4057d027596b8fea16b58220dd3b3af051f03346
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-08-10 11:41:04 -0400

    Revert "Log changed projects"
    This reverts commit c26683feb63544c9861850182f62b664eef49df9.

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -21,8 +21,6 @@ function projectChanged(currentProject, fromHash, toHash) {
   const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
   const output = execSync(getAffected).toString();
   //get the list of changed projects from the output
-  console.log("➡️ Here's the output:");
-  console.log({ output });
   const changedProjects = JSON.parse(output).projects;
   if (changedProjects.find((project) => project === currentProject)) {
     return true;
