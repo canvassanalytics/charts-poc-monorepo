ef6fa394777e180ae81143c0ae9be7ea285875ff
ayden-canvass <65177613+ayden-canvass@users.noreply.github.com>
2022-08-09 23:41:47 -0400

    Configure deployment for local dependancies (#8)

--- apps/echarts/src/app/components/BarPlot.js
+++ apps/echarts/src/app/components/BarPlot.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { generateBarData } from '@charts-poc-mono/data-utils';
 import EChart from './EChart';
