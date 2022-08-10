6733b329624c2172ac22834598fa7ce7b6859073
Ayden <ayden.garrett@canvass.io>
2022-08-09 23:38:47 -0400

    clean up

--- tools/plugins/netlify/install-deps/index.js
+++ tools/plugins/netlify/install-deps/index.js
@@ -2,9 +2,5 @@ module.exports = {
     onPreBuild: ({ utils }) => {
         const currentProject = process.env.PROJECT_NAME;
         utils.run.command(`npm install ./apps/${currentProject}`);
-        // utils.run.command(`npm link .apps/${currentProject}/node_modules/react`);
-        utils.run.command(`echo done`);
-        // utils.run.command(`pwd`);
-        // utils.run.command(`npm install`);
     },
 };
