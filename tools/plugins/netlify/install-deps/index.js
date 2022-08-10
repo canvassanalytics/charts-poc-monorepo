ba84c74e3a8c82c14b6133e2ca222e553309dfeb
Ayden <ayden.garrett@canvass.io>
2022-08-09 23:14:47 -0400

    update plugin

--- tools/plugins/netlify/install-deps/index.js
+++ tools/plugins/netlify/install-deps/index.js
@@ -1,7 +1,9 @@
 module.exports = {
     onPreBuild: ({ utils }) => {
         const currentProject = process.env.PROJECT_NAME;
+        utils.run.command(`npm install`);
         utils.run.command(`npm --prefix ./apps/${currentProject} install ./apps/${currentProject}`);
+        utils.run.command(`npm link .apps/${currentProject}/node_modules/react`);
         utils.run.command(`echo done`);
         // utils.run.command(`pwd`);
         // utils.run.command(`npm install`);
