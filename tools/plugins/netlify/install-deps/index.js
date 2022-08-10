92de5e8b97acfbb7bf614b04a5dfa25267a84ca1
Ayden <ayden.garrett@canvass.io>
2022-08-09 22:59:30 -0400

    again

--- tools/plugins/netlify/install-deps/index.js
+++ tools/plugins/netlify/install-deps/index.js
@@ -1,6 +1,9 @@
 module.exports = {
     onPreBuild: ({ utils }) => {
         const currentProject = process.env.PROJECT_NAME;
-        utils.run.command(`pwd && cd apps/${currentProject} && pwd && ls && npm install`)
+        utils.run.command(`pwd`);
+        utils.run.command(`cd apps/${currentProject}`);
+        utils.run.command(`pwd`);
+        utils.run.command(`npm install`);
     },
 };
