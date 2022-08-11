178c482c5176d1d000e477ceca355beec5d05394
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-08-11 09:59:25 -0400

    Add BarChart

--- apps/plotly/src/app/bar-chart/BarChart.js
+++ apps/plotly/src/app/bar-chart/BarChart.js
@@ -1,12 +1,59 @@
-import styled from 'styled-components';
-const StyledBarChart = styled.div`
-  color: pink;
-`;
-export function BarChart(props) {
+import styled from 'styled-components/macro';
+import { useState } from 'react';
+
+import { generateBarData } from '@charts-poc-mono/data-utils';
+
+import Plot from '../plotting';
+
+export default function LineChart(props) {
+  const [numberOfBars, setNumberOfBars] = useState(1000);
+
+  // Prepare data
+  const rawData = generateBarData(numberOfBars);
+
+  const xArray = [];
+  const yArray = [];
+
+  rawData.map(function ({ x, y }) {
+    xArray.push(x);
+    yArray.push(y);
+  });
+
   return (
-    <StyledBarChart>
-      <h1>Welcome to BarChart!</h1>
-    </StyledBarChart>
+    <div>
+      <ChartControls>
+        <div>
+          <label>Number of points:</label>
+          <input
+            type="number"
+            value={numberOfBars}
+            onChange={(e) => setNumberOfBars(e.target.value)}
+          />
+        </div>
+      </ChartControls>
+
+      <Plot
+        data={[
+          {
+            type: 'bar',
+            x: xArray,
+            y: yArray,
+          },
+        ]}
+        layout={{
+          title: 'Bar chart',
+        }}
+        config={{
+          scrollZoom: true,
+        }}
+      />
+    </div>
   );
 }
-export default BarChart;
+
+const ChartControls = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
