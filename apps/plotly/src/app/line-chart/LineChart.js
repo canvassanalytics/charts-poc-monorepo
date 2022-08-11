9e97021fb247ec69f9021d1f3d5625e4478071b2
Sergii Gatezh <gatezh@users.noreply.github.com>
2022-08-11 09:05:01 -0400

    Add Line Chart controls

--- apps/plotly/src/app/line-chart/LineChart.js
+++ apps/plotly/src/app/line-chart/LineChart.js
@@ -1,3 +1,4 @@
+import styled from 'styled-components/macro';
 import { useState } from 'react';
 
 import { generateTimeseriesData } from '@charts-poc-mono/data-utils';
@@ -5,21 +6,44 @@ import { generateTimeseriesData } from '@charts-poc-mono/data-utils';
 import Plot from '../plotting';
 
 export default function LineChart(props) {
+  const [numberOfPoints, setNumberOfPoints] = useState(1000);
   const [isShowRangeSlider, setIsShowRangeSlider] = useState(false);
+
+  // Prepare data for the line chart
+  const rawData = generateTimeseriesData(numberOfPoints, 60, true);
+
+  const xArray = [];
+  const yArray = [];
+
+  rawData.map(function ({ x, y }) {
+    xArray.push(x);
+    yArray.push(y);
+  });
 
   return (
     <div>
-      <label htmlFor="show-range-slider">
-        <input
-          type="checkbox"
-          id="show-range-slider"
-          checked={isShowRangeSlider}
-          onChange={() => {
-            setIsShowRangeSlider(!isShowRangeSlider);
-          }}
-        />
-        Show range slider
-      </label>
+      <ChartControls>
+        <div>
+          <label>Number of points:</label>
+          <input
+            type="number"
+            value={numberOfPoints}
+            onChange={(e) => setNumberOfPoints(e.target.value)}
+          />
+        </div>
+
+        <label htmlFor="show-range-slider">
+          <input
+            type="checkbox"
+            id="show-range-slider"
+            checked={isShowRangeSlider}
+            onChange={() => {
+              setIsShowRangeSlider(!isShowRangeSlider);
+            }}
+          />
+          Show range slider
+        </label>
+      </ChartControls>
 
       <Plot
         data={[
@@ -45,13 +69,9 @@ export default function LineChart(props) {
   );
 }
 
-// Prepare data for the line chart
-const rawData = generateTimeseriesData(1000, 60, true);
-
-const xArray = [];
-const yArray = [];
-
-rawData.map(function ({ x, y }) {
-  xArray.push(x);
-  yArray.push(y);
-});
+const ChartControls = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
